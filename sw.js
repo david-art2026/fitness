4546b715cd2f4e6a6fc64807016e87505630ca57
david-art2026 <1059022280@qq.com>
2026-09-10 12:38:54 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'hardcore-training-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存里有，直接从本地秒开；如果没有，再走网络
+        return response || fetch(event.request);
+      })
+  );
+});
